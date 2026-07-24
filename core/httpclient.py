bcdbcf47100a2df324a383350af7263ab2e18db3
ndavini9 <n.davini@9dot.it>
2026-07-24 17:45:06 +0200

    Update httpclient.py

--- core/httpclient.py
+++ core/httpclient.py
@@ -4,30 +4,38 @@ import time
 
 class HttpClient:
 
-   def __init__(
+    def __init__(
         self,
         protocol,
         host,
         port,
-        username,
-        password,
+        username=None,
+        password=None,
         timeout=10,
         verify_ssl=False
-):
+    ):
 
-    self.protocol = protocol
-    self.host = host
-    self.port = port
+        self.protocol = protocol
+        self.host = host
+        self.port = port
 
-    self.base_url = f"{protocol}://{host}:{port}"
+        self.base_url = (
+            f"{protocol}://{host}:{port}"
+        )
 
-    self.timeout = timeout
+        self.timeout = timeout
 
-    self.verify_ssl = verify_ssl
+        self.verify_ssl = verify_ssl
 
-    self.session = requests.Session()
+        self.session = requests.Session()
 
-    self.session.auth = (username, password)
+
+        if username and password:
+
+            self.session.auth = (
+                username,
+                password
+            )
 
 
     def request(
@@ -45,14 +53,23 @@ class HttpClient:
 
 
         response = self.session.request(
+
             method=method,
+
             url=url,
+
             json=json,
+
             data=data,
+
             headers=headers,
+
             timeout=self.timeout,
+
             verify=self.verify_ssl,
+
             allow_redirects=True
+
         )
 
 
@@ -62,48 +79,90 @@ class HttpClient:
 
 
         return {
+
             "response": response,
-            "duration_ms": elapsed,
+
+            "duration_ms": round(
+                elapsed,
+                2
+            ),
+
             "request": {
+
                 "method": method,
+
                 "url": url,
+
                 "json": json
+
             }
+
         }
 
 
-
-    def get(self, path, **kwargs):
+    def get(
+        self,
+        path,
+        **kwargs
+    ):
 
         return self.request(
+
             "GET",
+
             path,
+
             **kwargs
+
         )
 
 
-    def post(self, path, **kwargs):
+    def post(
+        self,
+        path,
+        **kwargs
+    ):
 
         return self.request(
+
             "POST",
+
             path,
+
             **kwargs
+
         )
 
 
-    def put(self, path, **kwargs):
+    def put(
+        self,
+        path,
+        **kwargs
+    ):
 
         return self.request(
+
             "PUT",
+
             path,
+
             **kwargs
+
         )
 
 
-    def delete(self, path, **kwargs):
+    def delete(
+        self,
+        path,
+        **kwargs
+    ):
 
         return self.request(
+
             "DELETE",
+
             path,
+
             **kwargs
+
         )
